229340810757e98ecdd167e0dd20931b93525441
Ender Ayalp <ayalp.ender@gmail.com>
2021-11-30 12:38:45 +0100

    New menue,shared module, empty sites Project and Home and new route config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from './layout/layout.module';
+import { HomeComponent } from './home/home.component';
+import { ProjectComponent } from './project/project.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HomeComponent, ProjectComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AppLayoutComponent } from './components/app-layout/app-layout.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [AppLayoutComponent, HeaderComponent],
+  exports: [AppLayoutComponent],
+  imports: [CommonModule, RouterModule, SharedModule],
+})
+export class LayoutModule {}
